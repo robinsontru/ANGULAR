dca514d51e675ca1e891b6d5766086afed49d839
robinsontru <robinsontruque04@gmail.com>
2023-05-15 12:16:45 -0500

    crd casi echo

--- src/app/SERVICES/equipo.service.ts
+++ src/app/SERVICES/equipo.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EquipoService {
-url='http://localhost:3001/api/';
+url='/api';
   constructor(private http:HttpClient) { }
 
 
@@ -14,4 +14,32 @@ getEquipos(){
   return this.http.get(this.url);
 }
 
+  //get un Equipo
+  getUnEquipo(id:string){
+    return this.http.get(this.url+'/'+id);
+  }
+
+
+  //agregar equipo
+  addEquipo(equipo:Equipo)
+  {
+    return this.http.post(this.url, equipo);
+  }
+
+
+  //eliminar
+  deleteEquipo(id:string){
+    return this.http.delete(this.url+'/'+id);
+  }
+
+  //modificar equipo
+  editEquipo(id:string, equipo:Equipo){
+    return this.http.put(this.url+'/'+id, equipo);
+  }
+
+}
+export interface Equipo{
+  id_equipo ?:string;
+  nombre?:string;
+  logo?:string;
 }
--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -1,15 +1,55 @@
-import { Component } from '@angular/core';
-import { EquipoService } from "../../SERVICES/equipo.service"
+import { Component, OnInit } from '@angular/core';
+import { Equipo,EquipoService } from "../../SERVICES/equipo.service";
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {
-  constructor(private EquipoService:EquipoService ){}
+export class InicioComponent implements OnInit {
+
+  //varibale
+  ListarEquipo: Equipo[] =[];
+
+constructor(private EquipoService:EquipoService, private router:Router  ){}
+
+  ngOnInit(): void {
+    this.listarEquipo();
+  }
 
 
-ngOnInit():void{
+  listarEquipo()
+  {
+    this.EquipoService.getEquipos().subscribe(
+      res=>{
+        console.log(res);
+        this.ListarEquipo=<any>res;
+      },
+      err => console.log(err)
+    );
+  }
 
+
+  eliminar(id:string)
+  {this.EquipoService.deleteEquipo(id).subscribe(
+  res=>{    console.log('equipo eliminado');
+    this.listarEquipo()   
+  },
+  err => console.log(err)
+  );}
+
+modificar(id:string){
+this.router.navigate(['/edit/'+id])
 }
+  
+  
+
+
+  
+ 
 }
+
+
+
+
+ 
